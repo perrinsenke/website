151d15f7b300317eba4f004cb782ec8139f2b0da
perrinsenke <perrinsenke@gmail.com>
2026-08-06 21:58:45 -0400

    Add files via upload

--- perrins-books/website/src/books/book3.js
+++ perrins-books/website/src/books/book3.js
@@ -74,5 +74,50 @@ export const poems = [
       "",
       "Practice, practice, preach, peachy. Down a pack of bold faced lies and teach me, preachy! Not you, sorry, behind you I mean. Sorry for the inconvenience. Not a bother at all. What DO you want? Practice makes perfect, so keep on wondering. Sleep on the treadmill, fall awake walking, riddle me or riddle you or trip over the drive and drop your dominos. Scratched up. Far out. Wax on. Practice makes perfect."
     ]
+  },
+  {
+    id: "poem-6",
+    title: "Burn the Rockstar",
+    displayTitle: "Burn the Rockstar",
+    lines: [
+      "One, two, three, four, perfect score, sporadic XL clouds cut through entirely. Pull a jack, get to it later. Dream in shades of Southwest, Northeast, right smack dab in the center. A dark blue, several dark blues, eye spy to your little eye a world where the lines are already written. Ironic. Predisposed. Common. Iconic. Cellular. Platonic. Get the picture? Flick and flack to and fro, Mustangs tally Blackjack. Bingo gumbo-limbo sliding window smack dab in the blood red hue of the known universe. Lots to cover. To cover, fully, comprehensively, argumentatively, wondering if it's worth it in the last place. Or the gold. So relaxed you don't even want to move. Standstill, drug addicted, clean as a whistle. A big car. A drenched dog. One hundred percent pure, diluted to shit. Addicted. Deal with it, or deal it in, or simply don't at all. One more apt spy scent brick cord, heart attack stroke alarm, just so you know. Click clack. Motherfucker, my lighter is dead in the water."
+    ]
+  },
+  {
+    id: "poem-7",
+    title: "Teen Dream // Take the Plunge // Coral",
+    displayTitle: "Teen Dream // Take the Plunge // Coral",
+    lines: [
+      "I saw the light glow last night, from the projector screen. Like the light of the mouse in the sky, like the aged gouda melted thinly over canvas, like the bumpy Uber ride home.",
+      "",
+      "I paint a thin smile on the page, ripped out familiarly. I trace the veins. White blank sheets fresh from wash, air of lavender across the landscape. Speedy and dark, lights drawing my eyes from the signs to the reservoir. Perpendicular. An old piano, slightly out of tune, rings through the church. And I'm on the corner, and you're on the roll, and we're both spread like a little too little butter over a little too much. Porridge. Any way you want it. The carriage wheel threatens to shoot off, but screw the screw. Wheel(s) only love once. Let ‘em fly.",
+      "",
+      "Drink down the harsh ale, let its warmth fill your stomach to mind. Shark attack, heart attack, locomotive, keep going. Power outage, I’m lighting a short fuse, I’m running on a short track, I’m dancing with an Angel. Send it. Evocative, push forward, push past the block, the clock, push past the rolling rocks for the sake of something more than a clever piece of candy. More. Why put off ‘til tomorrow what could be done today? Basic. Send it. The piano is deafening, the landscape makes me sick, the butter is full of mold and the porridge is burning through the fine china, fine! I rip paper from pad, wishing him after her into existence, out of existence. Classic. Send it."
+    ]
+  },
+  {
+    id: "poem-8",
+    title: "five minutes (too) late // making it up as we go",
+    displayTitle: "five minutes (too) late // making it up as we go",
+    lines: [
+      "My therapist looks across at me, having asked a question I didn’t hear. Likely she said something about my past, though it may well have been the present. My arms are heavy to my sides. My heart is beating in my chest. I’m focusing on the mirror behind her displaying the left half of my body and face, and I’m looking at everything in the mirror that isn't skin. I think that no cell was ever supposed to think this much about itself, and I realize it's been too long since the therapist’s question has entered the air. I appear deep in thought, taste my tongue in my mouth, wet copper tracing my teeth, I remember I am nothing but filth, I am nothing but the grime in the drain of the shower, I remember I am fear, and I remember that I am not my body. The mirror blurs as the whole room does, as the foreign object in my chest shoots across the room. I am nothing, and I was born this way."
+    ]
+  },
+  {
+    id: "poem-9",
+    title: "Lexicon",
+    displayTitle: "Lexicon",
+    lines: [
+      "Avarice. Avatar, above-all-else, acrobatic, insensible. Barbaric, battalion-based ballistic. Carnal. Crunching, crunching, candible, candific, fettel-ten-tennel, crunchy fennel and forward facing f-hole Frankensteins. Steinway, candify! Fervible. Foreign, flesh-walking, postulated and unattended, unattractive, untapped, fundamental enchantment, fervifice. Clean up, clean this all up, fervify, terrifying, liquid lieutenant, grape-filleted airborne. Liquible, liquifice, licorice, unaliveable. Unlivable. Livible. Red-line, hard-labour, search-bar sticks-n-stone, to-n-fro, livifice. Livify! Livid. Thesaurus. Lucible, crucible, sound-alike look-alike, smell-ya-l8r. Lucifice. Like a liquid lieutenant, grape-filleted sanctimonious. Lucify luxurious. Punt, to first, cuckoo, click loud, pallible pallifice, a wheel-stocked palace. Flow. Pallify. Palladium pendulum prognosis, pass-the-time. Rigible, eligible, clock-n-load, tuck-n-roll, rigifice. Rigify, rift-n-wise, footy ball mistake disguise. Stupible. Don't question it. Terrid, like an affair, torpible, not underwater nor unbeneath. U-boat white noise. Scream-n-shout. Present day torpifice, modern day utopía. Torpify. Thunderstorm, terrain-bomb, scatter-celled across the room. Tepible, tepifice, tepify. Class-clown call girl, crass-crown ball hog, small-jerk-heartthrob. Vastly more never, venerated generationally, regenerated potentiality. Vigible, by which I mean vigid, not rough and not finished, not timid, tough, rigid, one more time, vigifice, stupor, on-the-stoop, long-n-tired, stretched thin, fresh-thyme cuckoo-stucco-bamboo threshold. Dynamite! Vigify, verbatim."
+    ]
+  },
+  {
+    id: "poem-10",
+    title: "Adjourned // [REDACTED] // No Relation",
+    displayTitle: "Adjourned // [REDACTED] // No Relation",
+    lines: [
+      "2 weeks. Give or take. Call it a mulligan, call it a stroke, call it whatever you want. I’m all out of luck. A handful of darts, a copper-tale of drunks, a half-remembered half-hug halfway out the door. Toccata. Focaccia bread, crackers-and-cheese, bottle-o-whine, aero-planed. Count me out. Meeting concluded, long time coming, long while ending, long whale beached-beheaded, long-rhyme throat indebted. Call it a mulligan. Feel out, feel low, feel it out, blow-hole camel-pak. Nice to tear-of-meat you. Call me by your name, it's halfway out my lips, it's trickling down my throat and it's boring a hole in my ribs, between the cage, ball-and-chained, aperol spritzed. Leaf-lined, length-fined, snoring away the night. Call me a cab, I’m ready for",
+      "anywhere but here."
+    ]
   }
 ];
